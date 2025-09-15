2c55a7b559e37cc53aea0f58a192c73532c46963
001masonik <vladshpak.school@gmail.com>
2025-09-15 11:46:21 +0000

    3

--- js/main.js
+++ js/main.js
@@ -14,3 +14,18 @@
 });
 
 
+/*smoth croll*/
+    $("[data-scroll]").on("click",function(event){
+      event.preventDefault();
+      let elementId=$(this).data('scroll');
+      let elementOffset=$(elementId).offset().top;
+      // nav.removeClass("show");
+      
+      $("html, body").animate({
+        scrollTop:elementOffset -70
+      });
+      
+      
+    });
+
+
